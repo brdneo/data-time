8f48ccca48b3ebd2f8a1b29ba249d7d8a9d47623
brendo <brdneo@proton.me>
2025-05-12 23:04:59 -0300

    commitando

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import pandas as pd
+import kagglehub
+import os
+
+# handle do dataset e o arquivo
+dataset_handle = "mohammadtalib786/retail-sales-dataset"
+file_name_inside_dataset = "retail_sales_dataset.csv"
+
+# download do dataset
+dataset_directory = kagglehub.dataset_download(dataset_handle)
+
+# caminho completo para o csv
+full_path_to_file = os.path.join(dataset_directory, file_name_inside_dataset)
+
+df = pd.read_csv(full_path_to_file)
+
+print(df.head())
