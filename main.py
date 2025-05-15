59e14d733c73c3a7900f0be9264cd00800b105d6
brendo <brdneo@proton.me>
2025-05-14 21:13:22 -0300

    codiguinhos

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pandas as pd
-import kagglehub
+import kagglehub as kgh
 import os
 
 # handle do dataset e o arquivo
-dataset_handle = "mohammadtalib786/retail-sales-dataset"
-file_name_inside_dataset = "retail_sales_dataset.csv"
+dataset = "ky1338/10000-movies-letterboxd-data"
 
 # download do dataset
-dataset_directory = kagglehub.dataset_download(dataset_handle)
+dataset_directory = kgh.dataset_download(dataset)
+
+for f in os.listdir(dataset_directory):
+    print(f)
+
+files = os.listdir(dataset_directory)
+
+filename = "f"
 
 # caminho completo para o csv
-full_path_to_file = os.path.join(dataset_directory, file_name_inside_dataset)
+full_path_to_file = os.path.join(dataset_directory, filename)
 
-df = pd.read_csv(full_path_to_file)
+df = pd.read_csv(filename)
 
 print(df.head())
